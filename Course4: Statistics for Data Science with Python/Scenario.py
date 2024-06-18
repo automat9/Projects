c87f33e47c375fdb40f35c70216b63c5555204c3
Mateusz Pawlaczyk <168810098+Redd912@users.noreply.github.com>
2024-06-18 06:17:49 +0100

    Create Scenario.py

---
+++ Course4: Statistics for Data Science with Python/Scenario.py
@@ -0,0 +1,1 @@
+# I am a Data Scientist with a housing agency in Boston MA, you have been given access to a previous dataset on housing prices derived from the U.S. Census Service to present insights to higher management. 
